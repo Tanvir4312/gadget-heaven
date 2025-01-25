25085f85d04242ee61950a95c1e99bc0b852e8eb
Tanvirul islam <tanveer99ryan@gmail.com>
2025-01-26 02:28:15 +0600

    header and banner with responsive

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Error from './components/Error/Error';
+import Home from './components/Home/Home';
+import Root from './components/Root/Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
